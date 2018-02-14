9a48516f1bd63fc918e2b366b9af19c2a51794bc
canismail <ismail.can.meral1@gmail.com>
2018-02-14 18:58:46 +0000

    Update CoapConnect.java

--- CoapConnect.java
+++ CoapConnect.java
@@ -1,22 +1,22 @@
-    public static void main( String[] args )
-    {
+public static void main( String[] args )
+{
 
-	NetworkConfig.getStandard()
-		.setInt(NetworkConfig.Keys.MAX_MESSAGE_SIZE, 64)
-		.setInt(NetworkConfig.Keys.PREFERRED_BLOCK_SIZE, 64);
-    	URI uri = null; // URI parameter of the request
-    	
-    	// input URI from command line arguments
-	try {
-		uri = new URI("coap://[aaaa::212:7402:2:202]:5683");
-	} catch (URISyntaxException e) {
-		System.err.println("Invalid URI: " + e.getMessage());
-		System.exit(-1);
-	}
+NetworkConfig.getStandard()
+	.setInt(NetworkConfig.Keys.MAX_MESSAGE_SIZE, 64)
+	.setInt(NetworkConfig.Keys.PREFERRED_BLOCK_SIZE, 64);
+URI uri = null; // URI parameter of the request
 
-	CoapClient client = new CoapClient(uri);
+// input URI from command line arguments
+try {
+	uri = new URI("coap://[aaaa::212:7402:2:202]:5683");
+} catch (URISyntaxException e) {
+	System.err.println("Invalid URI: " + e.getMessage());
+	System.exit(-1);
+}
 
-	CoapResponse response = client.get();
+CoapClient client = new CoapClient(uri);
 
-	System.out.println(response.getResponseText());
-    }
+CoapResponse response = client.get();
+
+System.out.println(response.getResponseText());
+}
